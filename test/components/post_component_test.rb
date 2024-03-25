29abf11b7e62d1f5c88304ba37f46ead7056d2bf
AlbertoJTD <albertojtd@outlook.com>
2024-03-25 15:24:09 -0600

    Add PostComponent tests

--- test/components/post_component_test.rb
+++ test/components/post_component_test.rb
@@ -3,10 +3,24 @@
 require "test_helper"
 
 class PostComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(PostComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_component_renders_post_title
+    post = posts(:apple)
+    render_inline PostComponent.new(post:)
+    assert_text(post.title)
+  end
+
+  def test_component_renders_post_body
+    post = posts(:apple)
+    render_inline PostComponent.new(post:)
+    assert_text(post.body)
+  end
+
+  def test_component_renders_post_content
+    post = posts(:apple)
+    expected = 'Hello world' 
+    render_inline PostComponent.new(post:) do
+      expected  
+    end
+    assert_text(expected)
   end
 end
